93df89b0d73908f8ff3c8fb56106783cc9231eb5
PeterKahenya <kahenya0@gmail.com>
2018-12-11 02:26:26 +0300

    Final Commit

--- decrypt.py
+++ decrypt.py
@@ -1,1 +1,30 @@
-print("file decrpted")
+
+e=0
+n=0
+d=0
+def get_keys():
+    f=open("private_key.txt","r")
+    d=int(f.read())
+    f.close()
+    f=open("public_keys.txt","r")
+    keys=f.read().split(",")
+    f.close()
+    e=int(keys[0])
+    n=int(keys[1])
+    return e,n,d
+
+def get_cipher_text():
+    file=open("ciphertext.txt","r")
+    original=file.read()
+    cipher_t=[ord(x) for x in original]
+    return cipher_t
+
+def decrypt():
+    e,n,d=get_keys()
+    ciphertext=get_cipher_text()
+    plain = [chr((char ** d) % n) for char in ciphertext]
+    plain = ''.join(plain)
+    print("The message is :"+'\033[92m'+plain+'\033[0m')
+
+
+decrypt()
--- encrypt.py
+++ encrypt.py
@@ -1,1 +1,69 @@
-print("file encrypted")
+import random
+import time
+import math
+from math import gcd
+
+p=0
+q=0
+n=0
+e=0
+def isPrime(Number):
+    return 2 in [Number,2**Number%Number]
+
+def get_plain():
+    file=open("plaintext.txt","r")
+    original=file.read()
+    file.close()
+    plain_t=[ord(x) for x in original]
+    return plain_t
+
+def get_private(e,phiN,n):
+    for x in range(1,phiN*phiN):
+        if (e*x)%phiN==1:
+            return x
+
+def generate_keys():
+    random.seed(time.time())
+    primes = [i for i in range(1,100) if isPrime(i)]
+    p=random.choice(primes)
+    q=random.choice(primes)
+    n=p*q
+    phiN=(p-1)*(q-1)
+    e = random.randrange(1, phiN)
+    g = gcd(e, phiN)
+    while g != 1:
+        e = random.randrange(1, phiN)
+        g = gcd(e, phiN)
+    d=get_private(e,phiN,n)
+    #print(" p="+str(p)+" q="+str(q)+" n="+str(n)+" phiN="+str(phiN)+" e="+str(e)+" d="+str(d))
+    send_keys(e,n,d)
+    return e,n,d
+
+def encrypt():
+    e,n,p=generate_keys()
+    plain_t=get_plain()
+    cipher_t=[]
+    for p in plain_t:
+        c=(p**e)%n
+        cipher_t.append(c)
+    cipher=''.join([chr(i) for i in cipher_t])
+    send_cipher(cipher)
+    print("encryption "+'\033[92m'+ " DONE \u2713"+ '\033[0m')
+
+def send_cipher(cipher_t):
+    file=open("ciphertext.txt","w")
+    file.write(cipher_t)
+    file.close()
+
+def send_keys(e,n,p):
+    f=open("public_keys.txt","w")
+    keys=str(e)+","+str(n)
+    f.write(keys)
+    f.close()
+
+    f=open("private_key.txt","w")
+    keys=str(p)
+    f.write(keys)
+    f.close()
+
+encrypt()
